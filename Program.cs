3740dd53f29ff98d7e32fd48259ae00bbe227553
My_Name <myEmail@wherever.com>
2020-04-05 21:56:33 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string appName = "Number Guesser";
+            string appVersion = "1.0";
+            string appAuthor = "Malindu";
+        }
+    }
+}
