c7ea10237ede8bbbe073b66b2c7c8a575abb4e54
My_Name <myEmail@wherever.com>
2020-04-05 22:17:55 +0530

    first commit

--- Program.cs
+++ Program.cs
@@ -10,6 +10,9 @@ namespace Number
             string appName = "Number Guesser";
             string appVersion = "1.0";
             string appAuthor = "Malindu";
+
+            Console.WriteLine("{0}: Version {1} by {3}", appName, appVersion, appAuthor);
+            Console.WriteLine("Hello");
         }
     }
 }
